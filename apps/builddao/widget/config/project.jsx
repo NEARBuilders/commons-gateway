deea225dfc017ab4cd44173b257bcd1029cf228d
Jikugodwill <jikugodwill@yahoo.com>
2024-03-02 09:43:24 +0100

    Remove unused code and comments

--- apps/builddao/widget/config/project.jsx
+++ apps/builddao/widget/config/project.jsx
@@ -4,37 +4,31 @@ return {
     overview: {
       path: "buildhub.near/widget/components.project.page.Overview",
       blockHeight: "final",
-      init: {
+      init: {},
+    },
+    // discussion: {
+    //   path: "buildhub.near/widget/components.project.page.Discussion",
+    //   blockHeight: "final",
+    //   init: {
 
-      }
-    },
-    discussion: {
-      path: "buildhub.near/widget/components.project.page.Discussion",
-      blockHeight: "final",
-      init: {
-        
-      }
-    },
-    task: {
-      path: "buildhub.near/widget/components.project.page.Task",
-      blockHeight: "final",
-      init: {
-        
-      }
-    },
+    //   }
+    // },
+    // task: {
+    //   path: "buildhub.near/widget/components.project.page.Task",
+    //   blockHeight: "final",
+    //   init: {
+
+    //   }
+    // },
     code: {
       path: "buildhub.near/widget/components.project.page.Code",
       blockHeight: "final",
-      init: {
-        
-      }
+      init: {},
     },
     roadmap: {
       path: "buildhub.near/widget/components.project.page.Roadmap",
       blockHeight: "final",
-      init: {
-        
-      }
+      init: {},
     },
   },
 };
